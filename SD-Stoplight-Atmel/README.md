751c7aa94d922996090831caf71642ad3c815fba
Reilley Ford <reilleyford@gmail.com>
2020-10-02 08:29:58 -0400

    Updated Atmel Studio version to build/debug

---
+++ SD-Stoplight-Atmel/README.md
@@ -0,0 +1,42 @@
+# SD Stoplight #
+
+### Introduction ###
+This program is used to identify SD Cards `CMD42` lock status. It is part of a suite of tools used for  
+MMC/SD Card serial communication. Sending MMC specific commands and reading responses returned from  
+the card or device itself. All transactions are done via the `SPI` (Serial Peripheral Interface).   
+
+- - - -
+
+#### Setup ####
+This is a program written in `C` and compiled via `AVR-GCC` for the Microchip `ATTiny804`  
+The `ATTiny804` was flashed using `avrdude` and an `Arduino UNO` as a `UPDI` programmer  
+The Arduino programmer was setup using `JTAG2UPDI` created by ElTangas.  
+
+Link to repo: https://github.com/ElTangas/jtag2updi  
+
+There are no non-standard libraries used, everything is coded using standard C and AVR-GCC libraries. 
+
+
+#### Quick Reference ####
+When powered on the code will run in a loop polling for a button press. Once the button is pressed a LED startup sequence
+is run. Then the process begins. The SPI protocol is enabled and power is sent to the SD card reader. A initialization 
+command is run and if the card cannot be detected or the card fails initialization the program will end and the `Yellow`  
+LED will be turned on. If the initialization passes, the code will run further commands to prep the card for a read of the 
+password status register. If the card is unlocked the `Green` LED will be turned on. If the card is locked the `Red` LED
+will be turned on. Fairly simple program, the tool is used for verification of lock status.
+
+
+#### Basic Use: ####
+Plug in SD Card to card reader  
+Press button to begin process 
+
+LED Condition | Card Status
+--------------|------------
+Red LED       | Locked
+Yellow LED    | No Detect/Card Error
+Green LED     | Unlocked
+
+
+
+
+Created by Reilley Ford
--- SD-Stoplight-Atmel/SD-Stoplight-Atmel/main.c
+++ SD-Stoplight-Atmel/SD-Stoplight-Atmel/main.c
@@ -1,9 +1,49 @@
-#include <stdio.h>
-#include <string.h>
+/************************************************************************/
+/*                           SD Stoplight                               */
+/*																		*/
+/* This tool is one of a set of forensic tools within the framework of  */
+/* SD card CMD42 Interaction via Serial Peripheral Interface (SPI)		*/
+/* This SPI interaction is done from a Hardware Serial connection. In   */
+/* In this case a custom logic board, SD card I/O shield, LED Lights, a */
+/* switch, and an SD card.												*/
+/*																		*/
+/* This program was developed to validate the presence 	of a CMD42 lock	*/
+/* enabled on an SD card. SD Stoplight will setup the hardware lines,	*/
+/* LEDs, Switch Button, and SPI protocol. Once the hardware is setup    */
+/* the program enters an infinite loop polling the Switch waiting for a */
+/* state change. When the button is pressed and the state is changed	*/
+/* The program will begin it's process of attempting to initialize an   */
+/* SD card, perform the commands that are required for initialization   */
+/* If the card cannot initialize the Yellow light is flipped. If the	*/
+/* status of the card returns SD_OK then a read of the SD card registers*/
+/* is performed determining the status of the CMD42 lock.				*/
+/*																		*/ 
+/*						Basic Functionality								*/
+/*																		*/
+/*	Green:  Card Unlocked												*/
+/*	Yellow: No Card Detected/Non-Functional Card						*/
+/*	Red:	Card is CMD42 Locked										*/
+/*																		*/
+/*	This idea was created by:											*/
+/*	Benjamin Rutledge, Jeremy Dupuis, and Reilley Ford.  				*/
+/*																		*/
+/*																		*/
+/*	Code developed by:													*/
+/*	Reilley Ford														*/
+/*																		*/
+/*  Custom Board and Hardware created by:								*/
+/*  Benjamin Rutledge													*/
+/*																		*/
+/*																		*/
+/*  Honourable Mentions and Special Thanks to:							*/
+/*  Inspector Heath Crichton											*/
+/*  Detective Sergeant Rejean Carriere for support						*/
+/*	Tyler Burke, Matt Parker for being my Rubber Ducky					*/
+/************************************************************************/
+
 #include <ctype.h>
 #include <avr/io.h>
 #include <util/delay.h>
-#include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 
 #ifndef FALSE
@@ -11,12 +51,22 @@
 #define TRUE !FALSE
 #endif
 
-// Figure this out
+/*
+ * CPU Clock Speed - 16Mhz
+ */
+#ifdef F_CPU
+#undef F_CPU
+#define F_CPU 16000000
+#endif
+
+/*
+ * Baudrate setup.
+ */
 #define BAUDRATE    38400L
 #define BAUDREG     ((unsigned int)((F_CPU/(BAUDRATE*8UL))-1))
 
 /*
- * SD Card Commands
+ * SD Card Commands 
  */
 #define SD_IDLE        (0x40 + 0)   // CMD0: Set SD card to Idle
 #define SD_INIT        (0x40 + 1)   // CMD1: Initialize SD Card
@@ -30,34 +80,9 @@
 /*
  * Options for Types of SD cards.
  */
-#define  SDTYPE_UNKNOWN		0				/* card type not determined */
+#define  SDTYPE_UNKNOWN			0				/* card type not determined */
 #define  SDTYPE_SD				1				/* SD v1 (1 MB to 2 GB) */
 #define  SDTYPE_SDHC			2				/* SDHC (4 GB to 32 GB) */
-/*
- * Arduino is split into blocks of pins. Each block needs 3 Registers
- * DDR (Data Direction Register) - Dictates which pins are Input or output
- * PORT - Which block of pins is being used.
- * PIN - Reads input value when a pin is selected as Input mode.
- */
-
-// Setting up SPI and DDR (Data Direction Register)
-// DDR will decide whether the port is Input (0xFF) or output (Default and 0x00)
-// For example. Setting the fifth bit of DDRB to 1 means we are indicating that
-// we want to use the pin associated to the fifth bit in PORTB to be used as output.
-#define SPI_PORT  SPI0_CTRLA
-#define SPI_DDR   PORTA_DIRSET
-
-// Bits used by the SPI port
-#define MOSI  PORTA_PIN1CTRL
-#define MISO  PORTA_PIN2CTRL
-#define SCK   PORTA_PIN3CTRL
-// Fourth called SS for Slave Select, used for multiple slaves.
-
-// Definition for CS, port, and DDR for the SD Card. - Should match chip?
-#define SD_PORT     SPI0_CTRLA
-#define SD_DDR      PORTA_DIRSET
-#define SD_CS       PORTA_PIN4CTRL
-#define SD_CS_MASK  (1<<SD_CS)
 
 // Error codes for functions
 #define SD_OK         0
@@ -65,54 +90,60 @@
 #define SD_TIMEOUT    2
 #define SD_RWFAIL    -1
 
-// Definitions for button port/DDR/PIN
-#define SW_PORT PORTB
-#define SW_DDR  PORTB_DIRSET
-#define SW_PIN  PORTB_PIN0CTRL
+// Bits used by the SPI port
+#define MOSI  PIN1_bm // Master Out Slave In
+#define MISO  PIN2_bm // Master In Slave Out 
+#define SCK   PIN3_bm // Slave Clock
+#define CS	  PIN4_bm // Card Select/Slave Select.
 
-#define SW_BIT  0
-#define SW_MASK (1<<SW_BIT)
+// Setting up SPI and DDR (Data Direction Register)
+// DDR will decide whether the port is Input (0xFF) or output (Default and 0x00)
+// For example. Setting the fifth bit of DDRB to 1 means we are indicating that
+// we want to use the pin associated to the fifth bit in PORTB to be used as output.
+#define SPI_PORT  SPI0_CTRLA
+#define SPI_DDR	  PORTA_DIR
+#define SPI_OUT   PORTA_OUT
+
+// Mask for enabling Pullup on a pin and Input Sensing Configuration.
+#define PULLUP_MASK (1<<3)
+#define ISC_MASK	0x5 // Mask for Level Sensing of Pin (Generally Default)
+
+// Definitions for button port/DDR/PIN
+#define SW_DDR		PORTB_DIR
+#define SW_PIN		PORTB_PIN0CTRL
+#define SW_MASK		PIN0_bm
 
 // Definitions for RED LED
-#define LED_RED_PORT  PORTB				// Pulled low when button is pressed.
-#define LED_RED_DDR   PORTB_DIR			// Data Direction Register
+// Pulled low when button is pressed.
+#define LED_RED_DDR   PORTB_DIRSET		// Data Direction Register
 #define LED_RED_BIT   1				    // Set to HIGH when locked.
 #define LED_RED_MASK  (1<<LED_RED_BIT)
-#define LED_RED_OFF   (PORTB_OUT &= ~LED_RED_MASK)
-#define LED_RED_ON    (PORTB_OUT |= LED_RED_MASK)
-
-// Definitions for GREEN LED
-#define LED_GREEN_PORT PORTB
-#define LED_GREEN_DDR  PORTB_DIR
-#define LED_GREEN_BIT  2		       // Set to HIGH when unlocked.
-#define LED_GREEN_MASK (1<<LED_GREEN_BIT)
-#define LED_GREEN_OFF  (PORTB_OUT &= ~LED_GREEN_MASK)
-#define LED_GREEN_ON   (PORTB_OUT |= LED_GREEN_MASK)
+#define LED_RED_OFF   (PORTB_OUTCLR = LED_RED_MASK)
+#define LED_RED_ON    (PORTB_OUTSET |= LED_RED_MASK)
 
 // Definitions for YELLOW LED
-#define LED_YELLOW_PORT  PORTB
-#define LED_YELLOW_DDR   PORTB_DIR
-#define LED_YELLOW_BIT   3		      // Set to HIGH when unable to read/damaged card.
+#define LED_YELLOW_DDR   PORTB_DIRSET
+#define LED_YELLOW_BIT   2		      // Set to HIGH when unable to read/damaged card.
 #define LED_YELLOW_MASK (1<<LED_YELLOW_BIT)
-#define LED_YELLOW_OFF  (PORTB_OUT &= ~LED_YELLOW_MASK)
-#define LED_YELLOW_ON   (PORTB_OUT |= LED_YELLOW_MASK)
+#define LED_YELLOW_OFF  (PORTB_OUTCLR = LED_YELLOW_MASK)
+#define LED_YELLOW_ON   (PORTB_OUTSET |= LED_YELLOW_MASK)
+
+// Definitions for GREEN LED
+#define LED_GREEN_DDR  PORTB_DIRSET
+#define LED_GREEN_BIT  3		       // Set to HIGH when unlocked.
+#define LED_GREEN_MASK (1<<LED_GREEN_BIT)
+#define LED_GREEN_OFF  (PORTB_OUTCLR = LED_GREEN_MASK)
+#define LED_GREEN_ON   (PORTB_OUTSET |= LED_GREEN_MASK)
 
 // CMDs to run against SD Card
-#define  CMD_LOCK		 1
-#define  CMD_UNLOCK		 2
 #define  CMD_NONE		 3
 #define  CMD_INFO		 4
 #define  CMD_READBLK	 5
-#define  CMD_PWD_LOCK	 6
-#define  CMD_PWD_UNLOCK	 7
 #define  CMD_PWD_CHECK	 8
 #define  CMD_LOCK_CHECK  9
-#define  CMD_ERASE		 10
-#define  CMD_PWD_CLEAR   11
 
 // Global variables
 uint8_t sdtype;
-uint8_t block[512];
 uint8_t cardstatus[2];
 
 /*
@@ -122,25 +153,35 @@ static void     Select(void);
 static void     Deselect(void);
 static uint8_t  SendByte(uint8_t  c);
 static void     ProcessCommand(void);
-static uint8_t  ReadCommand(void);
 static int8_t   SendCommand(uint8_t  command, uint32_t  arg);
 static int8_t   InitializeSD(void);
 static int8_t   ReadSD(void);
-static int8_t   ReadStatus(void);
-static int8_t   WaitForData(void);
 
 int main(void) {
-
-  // First step, enable CS as output.
-  //SD_DDR
-  SPI_DDR |= SD_CS_MASK; // Setting the 2nd pin of PORTB (Chip Select) as output via DDRB
-  Deselect(); // Make sure card is not selected.
 	
-  SPI_PORT |= ((1<<MOSI) | (1<<SCK));   // Flip bits for MOSI and Serial Clock
-  SPI_DDR  |= ((1<<MOSI) | (1<<SCK));   // Mark pins as output
-  SPI_PORT |= (1<<MISO);                // Flipping MISO bit.
+  // Enable Global Interrupts
+  CPU_SREG = CPU_I_bm;
+ 
+  // Set SPI Data Direction Register. Drive outputs to SPI port MOSI and SCK.
+  SPI_DDR |= MOSI;	// MOSI -- Output
+  SPI_DDR &= ~MISO; // MISO -- Input
+  SPI_DDR |= SCK;	// SCK  -- Output
+  SPI_DDR |= CS;	// CS   -- Output 
+  Deselect(); // Deselect CS by driving output High.
+  PORTA_PIN2CTRL |= ISC_MASK | PULLUP_MASK; // MISO ISC and Pullup enable.
 
-
+ /**
+   * Enabling SPI via SPCR (Serial Peripheral Control Register)
+   * SPI_ENABLE_bm  - SPI Enable - Flip bit to enable SPI
+   * SPI_MASTER_bm - Master/Slave Select. If set Master mode is enabled.
+   * SPI_PRESC_DIV128_gc - Setting Clock Rate - Prescaler, Clock Speed is 16mhz - 20Mhz. 
+   * Prescaler divides by 128. Necessary because SPI maximum clock speed limitations.
+   * In this configuration Clock Rate is set to fosc/128.
+   **/
+  SPI_PORT = SPI_ENABLE_bm 
+		   | SPI_MASTER_bm 
+		   | SPI_PRESC_DIV128_gc; 
+		   
   // Setup hardware lines/pins
   // Red LED
   LED_RED_OFF;
@@ -156,88 +197,81 @@ int main(void) {
 
   // Switch setup - Marking as input
   SW_DDR &= ~SW_MASK;
-  SW_PIN |= SW_MASK;
-
-  /*
-   * Enabling SPI via SPCR (Serial Peripheral Control Register)
-   * SPE  - SPI Enable - Flip bit to enable SPI
-   * MSTR - Master/Slave Select. If set Master mode is enabled.
-   * SPR1 - Setting Clock Rate - Multiple options depending on SPX
-   * SPR0 - Setting Clock Rate - SPR0, SPR1 and SPI2X dictate Clock Rate based on which bits are set.
-   * In this configuration Clock Rate is set to fosc/128.
-   */
-  SPI_PORT |= (1<<SPI_ENABLE_bp) | (1<<SPI_MASTER_bp) | (SPI_PRESC_DIV128_gc);
-
-  while(1) ProcessCommand();
-
-  return 0;
+  SW_PIN |= ISC_MASK | PULLUP_MASK; // ISC and Pullup enable. Switch Pin
+ 
+ // Begin Infinite Loop polling switch to begin processing.
+  while(1) {
+	 if((PORTB_IN & SW_MASK) == 0) { 
+		LED_RED_OFF;
+		LED_YELLOW_OFF;
+		LED_GREEN_OFF;
+		_delay_ms(500);
+		LED_RED_ON;
+		_delay_ms(1000);
+		LED_YELLOW_ON;
+		_delay_ms(1000);
+		LED_GREEN_ON;
+		_delay_ms(2500);
+		LED_RED_OFF;
+		LED_GREEN_OFF;
+		LED_YELLOW_OFF;
+		ProcessCommand();
+	 }
+  }
+ 
+  return 0; // Should never happen.
 }
 
 /*
  * Flipping CS bit -- Selecting card.
+ * Pulled low for selection.
  */
 static void Select(void) {
-  SD_PORT &= ~SD_CS_MASK;
+	SPI_OUT &= ~CS;
 }
 
 /*
  * Flipping CS bit -- De-selecting card.
+ * Pulled High for deselection.
  */
 static void Deselect(void) {
-  SD_PORT |= SD_CS_MASK;
+	 SPI_OUT |= CS;
 }
 
 /*
  * ProcessCommand function
- * Beginning of code flow, kicked off by main(). This process loops awaiting
- * user input, either through switches or UART -- Some form of user input.
+ * Beginning of code flow.
+ * Attempt to Initialize SD
+ * Read SD for CMD42 Lock.
  */
 static void ProcessCommand(void) {
-  uint8_t         cmd, i;
-  static uint8_t  prevCMD = 0;
   uint8_t         response;
 
-  cmd = ReadCommand();
-
-  if((cmd != prevCMD) && (prevCMD == CMD_NONE)) {
-
   response = InitializeSD();
-  if(response != SD_OK) printf_P(PSTR("\n\r\n\rUnable to initialize card."));
-
+  if(response != SD_OK) {
+	LED_YELLOW_ON;
+	return;
+  }
+  
   /*
    * If card passes init vibe check, begin processing command.
    */
-   if(cmd == CMD_INFO) {
-     printf_P(PSTR("\r\nCard Type: %d"), sdtype);
-     response = ReadSD();
-     if(response == SD_OK) {
-
-     } else printf_P(PSTR("\r\nCard Registers could not be read."));
+   response = ReadSD();
+   if(response == SD_OK) {
+	   if(!(cardstatus[1] & 0x01)) LED_GREEN_ON; // No CMD42 lock detected.
+	   else if ((cardstatus[1] & 0x01) == 1) LED_RED_ON; // CMD42 lock detected and enabled.
+   } else {
+	  LED_YELLOW_ON;
+	  LED_GREEN_OFF;
+	  LED_RED_OFF;
    }
-  }
-  prevCMD = cmd;
-}
-
-/*
- * ReadCommand function
- * This is called during ProcessCommand and is used to determine CMD options/state
- * Returns CMD selected as response.
- */
-static uint8_t ReadCommand(void) {
-  uint8_t response;
-
-  _delay_ms(50);
-  response = CMD_NONE;
-  // Wait for data from UART.
-
-  return response;
 }
 
 /*
  * SD Card Initialization function.
  * This will begin by setting SD to idle mode.
  * Then it will probe the card to check for SDHC which requires ACMD41 interface
- * and advanced intialization methods.
+ * and advanced initialization methods.
  * Returns SD_OK or SD_NO_DETECT.
  */
 static int8_t InitializeSD(void) {
@@ -245,17 +279,22 @@ static int8_t InitializeSD(void) {
   int8_t response;
 
   sdtype = SDTYPE_UNKNOWN;
-
+  
   Deselect();
 
   // Send bytes while card stabilizes.
-  for(i=0; i < 10; i++) SendByte(0xff);
-
+  for(i=0; i < 74; i++) SendByte(0xff);
+  
   for(i = 0; i < 0x10; i++) {
     response = SendCommand(SD_IDLE, 0); // Try SD_IDLE until success or timeout.
     if(response == 1) break;
   }
-  if(response != 1) return SD_NO_DETECT;
+  if(response != 1) {
+	LED_YELLOW_ON;
+	LED_GREEN_OFF;
+	LED_RED_OFF;
+	return SD_NO_DETECT;
+  }
 
   SendCommand(SD_SET_BLK, 512); // Set block length to 512 bytes.
 
@@ -270,9 +309,8 @@ static int8_t InitializeSD(void) {
     sdtype = SDTYPE_SDHC;
   } else { // Begin initializing SDSC -- CMD1
     response = SendCommand(SD_OCR, 0); // Not necessary if voltage is set correctly.
-    if(response == 0x01) {
+    if(response == 0x01)
       for(i = 0; i < 4; i++) SendByte(0xff); // Burn the next 4 bytes returned (OCR)
-    }
     for(i = 20000; i > 0; i--) {
       response = SendCommand(SD_INIT, 0);
       if(response == 0) break;
@@ -289,22 +327,10 @@ static int8_t InitializeSD(void) {
 
 /*
  * ReadSD function
- * Kicks off a basic read of the available data registers.
- * OCR, CSD, CID.
+ * Reads the SD Status and returned registers. 
+ * Checks for CMD42 Lock.
  */
 static int8_t ReadSD(void) {
-   int8_t response;
-
-   response = ReadStatus();
-
-   return response;
-}
-
-/*
- * ReadStatus function
- * Reads the card status via CMD13
- */
-static int8_t ReadStatus(void) {
   cardstatus[0] = SendCommand(SD_STATUS, 0);
   cardstatus[1] = SendByte(0xff);
 
@@ -317,14 +343,14 @@ static int8_t ReadStatus(void) {
  * Function accepts an SD CMD and 4 byte argument.
  * Exchanges CMD and arg with CRC and 0xff filled bytes with card.
  * Returns the response provided by the card.
- * For advanced initilization and commands this will send the required preface CMD55
+ * For advanced initialization and commands this will send the required preface CMD55
  * Error codes will be 0xff for no response, 0x01 for OK, or CMD specific responses.
  */
 static int8_t SendCommand(uint8_t cmd, uint32_t arg) {
   uint8_t response, crc;
 
   /*
-   * Needed for SDC and advanced initilization.
+   * Needed for SDC and advanced initialization.
    * ACMD(n) requires CMD55 to be sent first.
    */
   if(cmd & 0x80) {
@@ -351,12 +377,13 @@ static int8_t SendCommand(uint8_t cmd, uint32_t arg) {
    if(cmd == SD_INTER) crc = 0x87;
    SendByte(crc);
 
-   // Send clocks waiting for timeout.
-   do {
-      response = SendByte(0xff);
-    } while((response & 0x80) != 0); // High bit cleared means OK
+   // Send clocks waiting for timeout. 
+	for (int i = 0; i<10; i++) {
+		response = SendByte(0xff);
+		if((response & 0x80) == 0) break; // High bit flipped means ok.
+	}
 
-   // Switch statement with fall through and default. Deselecting card if no more R/W operations required.
+   // Switch statement with fall through and default. De selecting card if no more R/W operations required.
    switch (cmd) {
      case SD_ADV_INIT :
      case SD_SET_BLK :
@@ -372,30 +399,20 @@ static int8_t SendCommand(uint8_t cmd, uint32_t arg) {
    return response;
 }
 
-
 /*
  * SendByte function.
- * ToDo: comment this.
+ * This function is the core of the SD card SPI interaction. 
+ * SendByte will write a Byte passed as an unsigned char c 
+ * This byte will be written to the SPI Data register
+ * The SPI Transfer Complete flag is then checked via Polling method
+ * Until the transfer is complete. The flag is then cleared after the flag and the Data register are read. 
+ * The SPI Data register is read for a response and the response is returned by the function. 
  */
 static unsigned char SendByte(unsigned char c) {
-  SPI0_DATA = c; // Write to SPI Data Register - Writes out to MOSI via Hosts SPI Bus
-  //while((SPSR & (1<<SPIF)) == 0); // Wait for SPSR and SPIF registers to clear.
-  return SPI0_DATA;
-}
 
-/*
- * WaitForData function
- * Used for commands that require processing and timeouts while awaiting response
- * that is not 0xff.
- */
-static int8_t WaitForData(void) {
-	int16_t				i;
-	uint8_t				response;
+	SPI0_DATA = c; // Write to SPI Data Register - Writes out to MOSI via Hosts SPI Bus
 
-	for (i = 0; i < 100; i++) {
-		response = SendByte(0xff);
-		if (response != 0xff) break;
-	}
+	while(!(SPI0_INTFLAGS & SPI_IF_bm)); // Wait until Transfer Complete flag is set. 
 
-	return  (int8_t) response;
+	return SPI0_DATA;
 }
